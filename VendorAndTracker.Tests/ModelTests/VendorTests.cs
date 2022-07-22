229cd5c0ffda79283b91475befb59f049b4cc142
Sandra Tena <sandratena06@gmail.com>
2022-07-22 13:09:31 -0700

    Add vendor test 1 and code

---
+++ VendorAndTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorAndTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorAndTracker.Tests
+{
+    [TestClass]
+    public class VendorTests : //IDisposable
+    {
+
+    // public void Dispose()
+    // {
+    //   Category.ClearAll();
+    // }
+
+      [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    }
+}
