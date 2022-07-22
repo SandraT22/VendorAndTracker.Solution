76d2391c62b68e57eb8af20a04574e2424712015
Sandra Tena <sandratena06@gmail.com>
2022-07-22 08:59:35 -0700

    Create initial files

---
+++ VendorAndTracker/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorAndTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorAndTracker.Controllers
+{
+  public class RecordsController : Controller
+  
+}
