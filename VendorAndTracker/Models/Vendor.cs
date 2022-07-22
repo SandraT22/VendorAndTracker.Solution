0bcf704e6d681d729bc8471ca7c493965b20e9eb
Sandra Tena <sandratena06@gmail.com>
2022-07-22 13:50:09 -0700

    fix typos on vendor.cs and pass first vendor test

--- VendorAndTracker/Models/Vendor.cs
+++ VendorAndTracker/Models/Vendor.cs
@@ -1,9 +1,18 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
-// namespace VendorAndTracker.Models
-// {
-//   public class Vendor
-//   {
-    
-//   }
-// }
+namespace VendorAndTracker.Models
+{
+  public class Vendor
+  {
+    public string VendorName { get; set; }
+    public string VendorAddress { get; set; }
+    public string VendorContact { get; set; }
+
+    public Vendor(string vendorName, string vendorAddress, string vendorContact)
+    {
+      VendorName = vendorName;
+      VendorAddress = vendorAddress;
+      VendorContact = vendorContact;
+    }
+  }
+}
